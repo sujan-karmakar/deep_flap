82d064d083383898eb2746e5fe825b0881540acf
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-07-28 11:29:53 +0530

    Best model save

--- agent.py
+++ agent.py
@@ -8,6 +8,9 @@ import yaml
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
+import os
+
 
 # Device setup
 if torch.backends.mps.is_available():
@@ -16,6 +19,10 @@ elif torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
+
+
+RUNS_DIR = "runs"
+os.makedirs(RUNS_DIR, exists_ok = True)
 
 
 class Agent:
@@ -40,6 +47,9 @@ class Agent:
         self.loss = nn.MSELoss()
         self.optimizer = None
 
+        self.LOG_FILE = os.path.join(RUNS_DIR, f"{self.param_set}.log")
+        self.MODEL_FILE = os.path.join(RUNS_DIR, f"{self.param_set}.pt")
+
 
 
     def run(self, is_training = True, render = False):
@@ -60,6 +70,12 @@ class Agent:
             steps = 0
             self.optimizer = optim.Adam(policy_dqn.parameters(), lr = self.alpha)
 
+            best_reward = float("-inf")
+
+        else:
+            policy_dqn.load_state_dict(torch.load(self.MODEL_FILE))
+            policy_dqn.eval()
+
         for episode in itertools.count():
             state, _ = env.reset()
             state = torch.tensor(state, dtype = torch.float, device = device)
@@ -67,7 +83,7 @@ class Agent:
             episode_reward = 0
             terminated = False
 
-            while not terminated:
+            while (episode_reward < self.reward_threshold and not terminated):
                 if is_training and random.random() < self.epsilon:
                     action = env.action_space.sample() # Explore
                     action = torch.tensor(action, dtype = torch.long, device = device)
@@ -90,7 +106,19 @@ class Agent:
 
             print(f"Episode: {episode + 1}, Reward: {episode_reward}")
 
+
             if is_training:
+                # Save best model
+                if episode_reward > best_reward:
+                    log_msg = f"Best reward: {episode_reward}"
+
+                    with open(self.LOG_FILE, "a") as f:
+                        f.write(log_msg + "\n")
+
+                    torch.save(policy_dqn.state_dict(), self.MODEL_FILE)
+                    best_reward = episode_reward
+
+
                 # Epsilon Decay:
                 epsilon = max(epsilon * self.epsilon_decay, self.epsilon_min)
 
@@ -98,7 +126,7 @@ class Agent:
                     # Get sample experiences
                     mini_batch = memory.sample(self.mini_batch_size)
 
-                    optimize(mini_batch, policy_dqn, target_dqn)
+                    self.optimize(mini_batch, policy_dqn, target_dqn)
 
                     if steps > self.network_sync_rate:
                         target_dqn.load_state_dict(policy_dqn.state_dict())
@@ -106,28 +134,16 @@ class Agent:
 
         #env.close() # Stop manually
 
-    
-    def optimize(self, mini_batch, policy_dqn, target_dqn):
-        # Get experience
-        state, action, next_state, reward, terminated = zip(*mini_batch)
 
-        state = torch.stack(state)
-        action = torch.stack(action)
-        next_state = torch.stack(next_state)
-        reward = torch.stack(reward)
-        terminated = torch.stack(terminated)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Train or test model")
+    parser.add_argument("hyperparameters", help = "")
+    parser.add_argument("--train")
+    args = parser.parse_args()
 
-        # Target Q
-        with torch.no_grad():
-            target_q = reward + (1 - terminated) * self.gamma * target_dqn(next_state).max(dim = 1)[0]
+    dqn = Agent(param_set = args.hyperparameters)
 
-        # Predicted Q
-        current_q = policy_dqn(state)
-
-        # Loss
-        loss = self.loss(current_q, target_q)
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
+    if args.train:
+        dqn.run(is_training = True)
+    else:
+        dqn.run(is_training = False)
